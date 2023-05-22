4b6adf3077a133a57033e14548704ca491aeb062
Guilhemfuel <guilhem.canivet@gmail.com>
2023-05-22 19:38:47 +0700

    Update PWA

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,23 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }, function(error) {
-      console.log('Service Worker registration failed:', error);
-    });
-  });
-}
+const staticDevCoffee = "chance-calculator-v1"
+
+const assets = [
+  "/",
+  "./index.html",
+  "./favicon/favicon-32x32.png",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
